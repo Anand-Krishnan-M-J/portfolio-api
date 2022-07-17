bc51f96b80c5fdc56b22bf7674e693e4100f8c3e
AKMJ <anandkrish0646@gmail.com>
2022-07-17 14:38:37 +0530

    Allow access from all origins

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,7 @@ var enableCORS = function (req, res, next) {
   }
 };
 app.all("/*", function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', ['http://localhost:4000','https://portfolio-anand-krishnan-m-j.vercel.app']);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, token, Content-Length, X-Requested-With, *');
   next();
