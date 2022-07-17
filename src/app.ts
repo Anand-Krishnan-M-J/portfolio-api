4963cb67a79b394dcac706d1d85c80d5f8fbdd58
AKMJ <anandkrish0646@gmail.com>
2022-07-17 14:46:56 +0530

    Allow access from all origins

--- src/app.ts
+++ src/app.ts
@@ -36,22 +36,5 @@ app.use(bodyparser.json());
 app.use("/users", userRoutes);
 app.use("/blogs", blogRoutes)
 export const server = app.listen(process.env.PORT || 3009, () => {
-    console.log("Server listening on PORT 3000");
+  console.log("Server listening on PORT 3000");
 });
-const sgMail = require('@sendgrid/mail')
-sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-const msg = {
-  to: 'anandkrishmj@gmail.com', // Change to your recipient
-  from: 'anandkrishmj@gmail.com', // Change to your verified sender
-  subject: 'Sending with SendGrid is Fun',
-  text: 'and easy to do anywhere, even with Node.js',
-  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-}
-sgMail
-  .send(msg)
-  .then(() => {
-    console.log('Email st')
-  })
-  .catch((error) => {
-    console.error(error)
-  })
