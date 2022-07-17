9420eba9e6bb7a803cb72b7f6418034856792e01
AKMJ <anandkrish0646@gmail.com>
2022-07-17 14:00:22 +0530

    Allow access from all origins

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ import blogRoutes from "./routes/blog";
 
 
 var enableCORS = function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', ['http://localhost:4000','https://portfolio-anand-krishnan-m-j.vercel.app']);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, token, Content-Length, X-Requested-With, *');
   if ('OPTIONS' === req.method) {
