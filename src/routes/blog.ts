c83c55007750492f8114ebf734c3fb6891612576
AKMJ <anandkrish0646@gmail.com>
2022-08-15 22:12:28 +0530

    Add authentication to add, edit or delete blogs

--- src/routes/blog.ts
+++ src/routes/blog.ts
@@ -5,8 +5,8 @@ const router = express.Router();
 
 router.get("/", controller.getBlogs);
 router.get("/:blogId", controller.getBlogById);
-router.post("/", controller.addBlog);
-router.put("/:blogId", controller.editBlog);
-router.delete("/:blogId", controller.deleteBlog);
+router.post("/", authenticate.authenticateJWT, controller.addBlog);
+router.put("/:blogId", authenticate.authenticateJWT, controller.editBlog);
+router.delete("/:blogId", authenticate.authenticateJWT, controller.deleteBlog);
 
 export default router;
