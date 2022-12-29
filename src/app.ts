8d861b2b453f6d44f2176a8d170c386ab193d5ce
AKMJ <anandkrish0646@gmail.com>
2022-12-29 23:55:19 +0530

    fix heroku bug

--- src/app.ts
+++ src/app.ts
@@ -11,24 +11,24 @@ import imageRoutes from "./routes/images";
 import emailRoutes from "./routes/email";
 import cors from 'cors'
 
-// var enableCORS = function (req, res, next) {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-//   res.header('Access-Control-Allow-Headers', 'Content-Type, token, Content-Length, X-Requested-With, *');
-//   if ('OPTIONS' === req.method) {
-//     res.sendStatus(200);
-//   } else {
-//     next();
-//   }
-// };
-// app.all("/*", function (req, res, next) {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-//   res.header('Access-Control-Allow-Headers', 'Content-Type, token, Content-Length, X-Requested-With, *');
-//   next();
-// });
+var enableCORS = function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, token, Content-Length, X-Requested-With, *');
+  if ('OPTIONS' === req.method) {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+};
+app.all("/*", function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, token, Content-Length, X-Requested-With, *');
+  next();
+});
 
-// app.use(enableCORS);
+app.use(enableCORS);
 
 const corsOptions = {
   origin: '*',
@@ -40,14 +40,14 @@ app.use(cors(corsOptions));
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
 
-// app.use("/users", userRoutes);
-// app.use("/blogs", blogRoutes);
-// app.use("/projects", projectRoutes);
-// app.use("/images", imageRoutes);
-// app.use("/email", emailRoutes)
+app.use("/users", userRoutes);
+app.use("/blogs", blogRoutes);
+app.use("/projects", projectRoutes);
+app.use("/images", imageRoutes);
+app.use("/email", emailRoutes)
 
-// export const server = app.listen(process.env.PORT || 3009, () => {
-//   console.log("Server listening on PORT 3009");
-// });
+export const server = app.listen(process.env.PORT || 3009, () => {
+  console.log("Server listening on PORT 3009");
+});
 
 
