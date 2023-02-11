6b4ee9b842921b1b3ddbd73c9776195da15d286e
AKMJ <anandkrish0646@gmail.com>
2023-02-12 00:09:04 +0530

    remove dep

--- src/middleware/user/validations.ts
+++ src/middleware/user/validations.ts
@@ -25,24 +25,24 @@ export const signUp = [
     }
 ];
 
-export const login = [
-    check("username", "username must be greater than 3 characters")
-        .custom(value => !/\s/.test(value))
-        .withMessage("No spaces are allowed in the username")
-        .isLength({
-            min: 3
-        })
-        .not()
-        .isEmpty(),
-    check("password", "Password length should be 4 to 20 characters").isLength({
-        min: 4,
-        max: 20
-    }).custom(value => !/\s/.test(value))
-    .withMessage("No spaces are allowed in the password"),
-    (req: Request, res: Response, next: NextFunction) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        } else next();
-    }
-];
+// export const login = [
+//     check("username", "username must be greater than 3 characters")
+//         .custom(value => !/\s/.test(value))
+//         .withMessage("No spaces are allowed in the username")
+//         .isLength({
+//             min: 3
+//         })
+//         .not()
+//         .isEmpty(),
+//     check("password", "Password length should be 4 to 20 characters").isLength({
+//         min: 4,
+//         max: 20
+//     }).custom(value => !/\s/.test(value))
+//     .withMessage("No spaces are allowed in the password"),
+//     (req: Request, res: Response, next: NextFunction) => {
+//         const errors = validationResult(req);
+//         if (!errors.isEmpty()) {
+//             return res.status(400).json({ errors: errors.array() });
+//         } else next();
+//     }
+// ];
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,12 +1,12 @@
 import express from "express";
-import { verifySignUp } from "../middleware/user/verifySignUp";
-import * as validate from "../middleware/user/validations";
+// import { verifySignUp } from "../middleware/user/verifySignUp";
+// import * as validate from "../middleware/user/validations";
 import * as authenticate from "../middleware/authentication";
 import * as controller from "../controllers/user.controller";
 const router = express.Router();
 
 // router.post("/signup",validate.signUp,verifySignUp.checkDuplicateUsernameOrEmail, controller.userSignup);
-router.post("/login", validate.login, controller.userLogin);
+router.post("/login", controller.userLogin);
 // router.get("/profile", authenticate.authenticateJWT, controller.getProfile);
 
 export default router;
