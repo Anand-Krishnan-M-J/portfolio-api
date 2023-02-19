1dba64324f806c0af54c0907721ead4422cefdb2
AKMJ <anandkrish0646@gmail.com>
2023-02-19 20:26:15 +0530

    remove node-modules

--- build/request format/blogs.js
+++
@@ -1,9 +0,0 @@
-const AddBlog = {
-    "title": "How to create a portfolio site",
-    "date": "2022-06-01",
-    "shortDescription": "Every developer must need a portfolio site to showcase the skills one has. Read on to find out how to build one",
-    "content": "Every developer must need a portfolio site to showcase the skills one has. Read on to find out how to build one",
-    "image": "https://images4.alphacoders.com/936/936378.jpg",
-    "isDeleted": "false"
-};
-//# sourceMappingURL=blogs.js.map
